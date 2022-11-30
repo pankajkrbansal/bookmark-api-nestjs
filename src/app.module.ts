e441646d68e4ecbe323943840f8356a8bffab362
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-12-01 01:40:14 +0530

    Added postgres setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import entities from './entities/entityIndex';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (cs: ConfigService) => ({
+        type: 'postgres',
+        host: cs.get('DB_HOST'),
+        username: cs.get('DB_USERNAME'),
+        password: cs.get('DB_PASSWORD'),
+        database: cs.get('DB_NAME'),
+        entities: entities,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+    name: 'user_id',
+  })
+  id: number;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  username: string;
+}
